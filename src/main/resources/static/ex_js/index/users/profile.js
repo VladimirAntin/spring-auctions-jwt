6ce79195ff2d947e7ed9086cfb7b3b672e59cb85
Vladimir_Antin <antin502@gmail.com>
2017-05-21 19:55:25 +0200

    change password fixed

--- src/main/resources/static/ex_js/index/users/profile.js
+++ src/main/resources/static/ex_js/index/users/profile.js
@@ -122,42 +122,6 @@ function Profile($scope,$http,$routeParams,$mdDialog,$mdToast) {
             locals:{user:$scope.user},
             controller: ChangePasswordController
         });
-        function ChangePasswordController($scope,data,$mdDialog,user,$routeParams,$mdToast) {
-            $scope.user =user;
-            $scope.btn_save_password = false;
-            $scope.save_password = function() {
-                $http({
-                    method: 'PATCH',
-                    url: '/api/users/'+$routeParams.userId+"/password",
-                    headers: {
-                        "Content-type":"application/json",
-                        "Authorization":$scope.token
-                    },
-                    data:$scope.user
-                }).then(function(response) {
-                    if(response.status==200){
-                        toast_message("Password was changed!","Ok",$mdToast);
-                        $mdDialog.hide();
-                    }
-                },function (response) {
-                    if(response.status==401){
-                        toast_message("Unauthorized","Ok",$mdToast);
-                    }else if(response.status>=500){
-                        toast_message("Server error","Ok",$mdToast);
-                    }
-                });
-            };
-            $scope.cancel = function() {
-                $mdDialog.hide();
-            };
-            $scope.check_password = function(change_password) {
-                if(change_password.$valid){
-                    $scope.btn_save_password = true;
-                }else{
-                    $scope.btn_save_password = false;
-                }
-            };
-        }
     };
 
     $scope.change_photo = function () {
