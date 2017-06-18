bd44d4f3d29cc6b573aa8b8e847f3157695499c4
Vladimir_Antin <antin502@gmail.com>
2017-06-18 23:41:35 +0200

    cors filter

--- src/main/java/org/auctions/sf57/security/JwtFilter.java
+++ src/main/java/org/auctions/sf57/security/JwtFilter.java
@@ -30,24 +30,26 @@ public class JwtFilter extends GenericFilterBean {
         final HttpServletRequest request = (HttpServletRequest) req;
         final HttpServletResponse response =(HttpServletResponse) res;
         final String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith("jwt ")) {
-            response.sendError(403,"Missing or invalid Authorization header.");
-            return;
-            //throw new ServletException("Missing or invalid Authorization header."); //500
+        if (!request.getMethod().equals("OPTIONS")){
+            if (authHeader == null || !authHeader.startsWith("jwt ")) {
+                response.sendError(403,"Missing or invalid Authorization header.");
+                return;
+                //throw new ServletException("Missing or invalid Authorization header."); //500
+            }
+            final String token = authHeader.substring(4); // The part after "jwt "
+
+            try {
+                final Claims claims = Jwts.parser().setSigningKey("secretkey")
+                        .parseClaimsJws(token).getBody();
+                request.setAttribute("claims", claims);
+            }
+            catch (Exception e) {
+                response.sendError(401,"Invalid token.");
+                return;
+                //throw new ServletException("Invalid token."); //500
+            }
         }
 
-        final String token = authHeader.substring(4); // The part after "jwt "
-
-        try {
-            final Claims claims = Jwts.parser().setSigningKey("secretkey")
-                .parseClaimsJws(token).getBody();
-            request.setAttribute("claims", claims);
-        }
-        catch (Exception e) {
-            response.sendError(401,"Invalid token.");
-            return;
-        //throw new ServletException("Invalid token."); //500
-        }
 
         chain.doFilter(req, res);
     }
